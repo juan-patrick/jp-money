1c3fda17cb125d209b329b174436d0155b953f7f
Juan Patrick <juan-p.andrade@outlook.com>
2022-05-05 07:02:07 -0300

    styles(global): add default font type

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,5 @@
 import { createGlobalStyle } from "styled-components";
 
-
 export const GlobalStyle = createGlobalStyle`
   :root{
     --red: #E52E4D ;
@@ -29,6 +28,16 @@ export const GlobalStyle = createGlobalStyle`
     @media(max-width: 720px){
       font-size: 87.5%; 
     }
+  }
+
+  body, input, textarea, button{
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+    
+  } 
+
+  h1,h2,h3,h4,h5,h6,strong{
+    font-weight: 600; 
   }
 
   button{
