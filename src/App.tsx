b6ff01c3135f9864dbed90df1de7c09bf2d764e3
Juan Patrick <juan-p.andrade@outlook.com>
2022-05-05 06:54:35 -0300

    styles(global): create global styles using styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import styled from 'styled-components';
+import { GlobalStyle } from './styles/global';
+
 export function App() {
   return (
     <div className="App">
+      <h1>
+        Hello World!
+      </h1>
+      <GlobalStyle />
     </div>
   );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from "styled-components";
+
+
+export const GlobalStyle = createGlobalStyle`
+  :root{
+    --red: #E52E4D ;
+    --blue: #5429cc;
+
+    --blue-light: #6933ff;
+
+    --text-tile: #363f5f;
+    --text-body: #969cb3;
+
+    --background: #f0f2f5;
+    --shape: #ffff;
+  }
+
+  *{
+    padding:0;
+    margin:0;
+    box-sizing: border-box;
+  }
+
+  html{
+    @media(max-width: 1080px){      
+      font-size: 93.75%; 
+    }
+
+    @media(max-width: 720px){
+      font-size: 87.5%; 
+    }
+  }
+
+  button{
+    cursor: pointer;
+  }
+
+
+  [disabled]{
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+
+  body{
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+`;
