eb40d70fac7744841abf561eca1973645a84a2b6
Juan Patrick <juan-p.andrade@outlook.com>
2022-05-06 06:33:32 -0300

    feat(components): add dashboard and summary components

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,14 @@
+import { Container } from "./styles"
+
+export function Summary() {
+
+  return (
+    <Container>
+      <div>
+        <header>
+          
+        </header>
+      </div>
+    </Container>
+  );
+}
