127eca849de3b527044cca20cec1347bef17b06d
Juan Patrick <juan-p.andrade@outlook.com>
2022-05-12 08:58:07 -0300

    feat(contexts/TransactionsContext): create transactions context and use in components

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { useState } from 'react';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsContext, TransactionsProvider } from './contexts/TransactionsContext';
 import { GlobalStyle } from './styles/global';
 
 export function App() {
 
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+
 
   function handleOpenNewTransactionModal() {
     setIsNewTransactionModalOpen(true);
@@ -17,7 +20,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <NewTransactionModal
@@ -27,6 +30,6 @@ export function App() {
       <Dashboard />
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
