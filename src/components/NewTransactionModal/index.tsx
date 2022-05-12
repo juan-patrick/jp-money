8ca01eb3a1d5ca0d373104dd1c4d1a6a2b333502
Juan Patrick <juan-p.andrade@outlook.com>
2022-05-12 09:12:52 -0300

    feat(contexts/TransactionsContext): add create transaction function in context

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,8 +3,9 @@ import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { FormEvent, useState } from "react";
+import { FormEvent, useContext, useState } from "react";
 import { api } from "../../services/api";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -16,17 +17,22 @@ ReactModal.setAppElement('#root');
 export function NewTransactionModal({
   isOpen, onRequestClose
 }: NewTransactionModalProps) {
+  const { transactions, createTransaction } = useContext(TransactionsContext);
+
   const [title, setTitle] = useState('');
   const [category, setCategory] = useState('');
-  const [value, setValue] = useState(0);
+  const [amount, setAmount] = useState(0);
   const [type, setType] = useState('deposit');
 
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    const data = { title, value, category, type };
-
-    api.post('/transactions', data);
+    createTransaction({
+      title,
+      amount: amount,
+      category,
+      type
+    })
   }
 
   return (
@@ -49,7 +55,7 @@ export function NewTransactionModal({
         <h2>Cadastrar Transação</h2>
 
         <input type="text" placeholder="Título" value={title} onChange={event => setTitle(event.target.value)} />
-        <input type="number" placeholder="Valor" value={value} onChange={event => setValue(Number(event.target.value))} />
+        <input type="number" placeholder="Valor" value={amount} onChange={event => setAmount(Number(event.target.value))} />
         <TransactionTypeContainer>
           <RadioBox
             type="button"
