b3930d9cca83fab679a61ca8f2a63c1a4cab3441
Juan Patrick <juan-p.andrade@outlook.com>
2022-05-16 06:51:09 -0300

    refactor(contexts/TransactionContext): remove transaction context and create hook useTransaction

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { useState } from 'react';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
-import { TransactionsContext, TransactionsProvider } from './contexts/TransactionsContext';
+import { TransactionsProvider } from './hooks/useTransactions';
 import { GlobalStyle } from './styles/global';
 
 export function App() {
-
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
-
 
 
   function handleOpenNewTransactionModal() {
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,10 +3,8 @@ import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { FormEvent, useContext, useState } from "react";
-import { api } from "../../services/api";
-import { TransactionsContext } from "../../contexts/TransactionsContext";
-
+import { FormEvent, useState } from "react";
+import { useTransactions } from "../../hooks/useTransactions";
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -17,7 +15,7 @@ ReactModal.setAppElement('#root');
 export function NewTransactionModal({
   isOpen, onRequestClose
 }: NewTransactionModalProps) {
-  const { createTransaction } = useContext(TransactionsContext);
+  const { createTransaction } = useTransactions();
 
   const [title, setTitle] = useState('');
   const [category, setCategory] = useState('');
